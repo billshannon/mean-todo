a1f10a387d8d2460ea4ccc8e026f3e17dbc4b860
Bill Shannon <bill@billshannon.com>
2016-04-05 15:43:06 -0600

    final commit

--- app/app.js
+++ app/app.js
@@ -5,3 +5,9 @@ var angular = require('angular');
 angular.module('todoListApp', []);
 
 require('./scripts/controllers/main.js');
+
+require('./scripts/controllers/todo.js');
+
+require('./scripts/services/data.js');
+
+require('./scripts/directives/todo.js');
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,16 +3,28 @@
 var angular = require('angular');
 
 angular.module('todoListApp')
-.controller('mainCtrl', function($scope, dataService){
-  
-  dataService.getTodos(function(response){
-    var todos = response.data.todos;
-    $scope.todos =  todos;
+  .controller('mainCtrl', function ($scope, $log, $interval, dataService) {
+
+    $scope.seconds = 0;
+
+    $scope.counter = function () {
+      $scope.seconds++;
+      $log.log($scope.seconds + ' have passed');
+    };
+
+
+    $interval($scope.counter, 1000, 10);
+
+    dataService.getTodos(function (response) {
+      var todos = response.data.todos;
+      $scope.todos = todos;
     });
-  
-  $scope.addTodo = function() {
-    $scope.todos.unshift({name: "This is a new todo.",
-                      completed: false});
-  };
-  
-})
+
+    $scope.addTodo = function () {
+      $scope.todos.unshift({
+        name: "This is a new todo.",
+        completed: false
+      });
+    };
+
+  })
