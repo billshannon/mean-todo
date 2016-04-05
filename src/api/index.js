6568e2dd47d6c54416a4a12c42830bc09534bef3
Bill Shannon <bill@billshannon.com>
2016-04-05 10:02:32 -0600

    mean-todo

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var express = require('express');
+var Todo = require('../models/todo');
+// var todos = require('../../mock/todos.json');
+var router = express.Router();
+
+router.get('/todos', function (req, res) {
+  Todo.find({}, function (err, todos) {
+    if (err) {
+      return res.status(500).json({message: err.message})
+    }
+    res.json({todos: todos});
+  })
+});
+
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 'use strict';
 
 var express = require('express');
+var router = require('./api');
 
 var app = express();
 
-app.use('/', express.static('public/public'));
+require('./database');
+require('./seed');
+
+app.use('/', express.static('public'));
+
+app.use('/api', router);
 
 app.listen(3000, function () {
   console.log('Port 3000 up and running!')
